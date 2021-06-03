ff269c2f166315ea4abe58c40a22888c78655c27
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-03 15:06:42 +0200

    added cors

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 3000
 require('dotenv').config()
 const userRouter = require('./routes/users')
 const authRouter = require('./routes/auth')
+const cors = require('cors')
+app.use(cors())
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 mongoose.connect(process.env.MONGO_DB, {useNewUrlParser : true, useUnifiedTopology : true})
 
